199a4313c5a72ebfe8e37c53a5b9cae9610546af
Abdessamad Bouabbadi <abouabba@e1r2p11.1337.ma>
2025-05-04 06:51:19 +0100

    update in parse and bultin ft_cd

--- buildin/ft_cd.c
+++ buildin/ft_cd.c
@@ -1,13 +1,49 @@
 #include "../minishell.h"
 
-int ft_cd(char **arg)
+char	*global_var(char set_it, char *pwd_var)
 {
-    (void)arg;
-    printf("cd\n");
-    // if (chdir(path) == -1)
-    // {
-    //     perror("cd");
-    //     return (1);
-    // }
-    return (0);
+	static char	*pwd___;
+
+	if (set_it)
+		pwd___ = pwd_var;
+	return (pwd___);
+}
+
+int ft_cd(char **args)
+{
+	const char	*path_____;
+    char		cwd[PATH_MAX];
+	if (!args[1] || strcmp(args[1], "~") == 0)
+	{
+		path_____ = getenv("HOME");
+		if (!path_____)
+		{
+			perror("cd: HOME not set");
+			return (1);
+		}
+	}
+	else if (strcmp(args[1], "-") == 0)
+	{
+		path_____ = getenv("OLDPWD");
+		if (!path_____)
+		{
+			perror("cd: OLDPWD not set");
+			return (1);
+		}
+		printf ("%s\n", path_____);
+	}
+	else
+		path_____ = args[1];
+
+	if (chdir(path_____) != 0)
+	{
+		perror("cd");
+		return (1);
+	}
+	if (getcwd(cwd, sizeof(cwd)))
+	{
+        setenv("OLDPWD", getenv("PWD"), 1);
+        setenv("PWD", cwd, 1);
+	}
+	return (0);
 }
